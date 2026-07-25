debc1eb89e4f52d663669884fa84848d9293afb4
aozijx <xxhtxml@outlook.com>
2026-07-25 21:56:39 +0800

    refactor: cleanup unused Preview annotation
    - **Code Cleanup**
        - Removed the unused `@Preview` annotation from the `EmptyVaultPlaceholder` composable.

--- app/src/main/java/com/aozijx/passly/feature/vault/components/EmptyVaultPlaceholder.kt
+++ app/src/main/java/com/aozijx/passly/feature/vault/components/EmptyVaultPlaceholder.kt
@@ -15,11 +15,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aozijx.passly.R
 
-@Preview
 @Composable
 fun EmptyVaultPlaceholder() {
     Column(
